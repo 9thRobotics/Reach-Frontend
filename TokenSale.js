b9b95694b55183b6d9e15ddda2c24b1ef2023171
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-16 16:29:31 -0800

    Update TokenSale.js

--- TokenSale.js
+++ TokenSale.js
@@ -1,29 +1,3 @@
-import React, { useState } from "react";
-
-function TokenSale() {
-  const [amount, setAmount] = useState("");
-
-  const handlePurchase = async () => {
-    alert(`You are purchasing ${amount} tokens!`);
-    // Add logic to interact with your smart contract here
-  };
-
-  return (
-    <div>
-      <h3>Buy Reach Tokens</h3>
-      <input
-        type="number"
-        placeholder="Enter ETH amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <button onClick={handlePurchase}>Purchase</button>
-    </div>
-  );
-}
-
-export default TokenSale;
-
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import useContract from './useContract';
@@ -61,6 +35,7 @@ function TokenSale() {
 }
 
 export default TokenSale;
+;
 
 
 
