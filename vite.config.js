9e39e6ae45f0df383e0de35176fd4256ba56a4cc
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-16 21:31:35 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite configuration for React
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Default port for Vite development server
+    port: 5173
   },
-  build: {
-    outDir: 'dist', // Output folder for production builds
-  }
+  base: '/Reach-Frontend/' // This ensures GitHub Pages serves the app correctly
 });
