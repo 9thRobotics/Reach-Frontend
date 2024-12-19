3ef6f0db1e8262b417f82b3ddde0793b4601a69d
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-18 21:02:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+console.log("Node.js is running successfully!");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
