d9c6eb836fe2e98f4db09ba4024118991d43f591
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-19 16:04:03 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reach-Frontend/', // Adjust this to match your GitHub repository name if deploying to GitHub Pages
   server: {
-    port: 5173, // Default Vite development server port
+    port: 5173,
     proxy: {
-      '/api': 'http://localhost:3000', // Proxy API requests to your backend
+      '/api': 'http://localhost:3000',
     },
   },
+  base: '/Reach-Frontend/'
 });
