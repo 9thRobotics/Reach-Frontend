8d05b80068aa309476a65cd84a655347844e3d64
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-03 14:12:49 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [react()],
     server: {
-        host: 'localhost', // Ensure the server listens on localhost
-        port: 5173,        // Change port to 5173
+        host: 'localhost',
+        port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:3000', // Backend API URL
+                target: 'http://localhost:3000',
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Clean up paths
+                rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
     },
