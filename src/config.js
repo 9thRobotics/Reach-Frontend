e5cd8f7be612e887030fa0f337b84d87985d6da6
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-05 20:05:15 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://new-reach-backend.herokuapp.com" // Production URL
-    : "http://localhost:5000"; // Local development URL
+    ? "https://new-reach-backend.herokuapp.com"
+    : "http://localhost:5000";
 
-   export const API_BASE_URL = "https://new-reach-backend.herokuapp.com";
-
- export default API_BASE_URL;
+export default API_BASE_URL;
