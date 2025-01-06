33c083da207265ff26cb6ea85d14886f9307e60b
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-05 21:31:19 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
-    plugins: [react()],
-    server: {
-        host: 'localhost',
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'https://new-reach-backend.herokuapp.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
+  base: '/',
+  plugins: [react()],
+  server: {
+    host: process.env.VITE_HOST || 'localhost',
+    port: process.env.VITE_PORT || 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'https://new-reach-backend.herokuapp.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
+  },
 });
