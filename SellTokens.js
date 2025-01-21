24d8f250b1f18c7a178973eed1b4f42c20c8f866
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-20 21:24:29 -0800

    Update SellTokens.js

--- SellTokens.js
+++ SellTokens.js
@@ -14,7 +14,7 @@ const SellTokens = ({ userWallet }) => {
 
         setIsSelling(true);
         try {
-            const response = await fetch("https://your-backend-url.com/sell", {
+            const response = await fetch("https://new-reach-backend-9th-robotics-s-projects.vercel.app/", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
