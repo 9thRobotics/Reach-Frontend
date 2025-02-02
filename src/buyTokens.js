711232bddc41dd9876c591aea2e89d6104895d73
9thRobotics <phillipdfilkins@hotmail.com>
2025-02-01 21:44:59 -0800

    Update buyTokens.js

--- src/buyTokens.js
+++ src/buyTokens.js
@@ -10,18 +10,10 @@ if (window.ethereum) {
     console.error("❌ No Ethereum provider found. Please install MetaMask.");
 }
 
-// Get contract address from environment variable
-let contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+// Updated contract address
+const contractAddress = "0x379d30d72a103b58cF00A6F5f8DBfe03C7bbf5Ef";
 
-// Validate and checksum the contract address
-if (!contractAddress || !web3.utils.isAddress(contractAddress)) {
-    console.error("❌ Invalid contract address:", contractAddress);
-} else {
-    contractAddress = web3.utils.toChecksumAddress(contractAddress);
-    console.log("✅ Checksummed Contract Address:", contractAddress);
-}
-
-// Contract ABI (Ensure this is correct)
+// Contract ABI
 const contractABI = [
     {
         "inputs": [],
@@ -44,6 +36,99 @@ const contractABI = [
             { "internalType": "uint256", "name": "value", "type": "uint256" }
         ],
         "name": "approve",
+        "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" },
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
+        "name": "allowance",
+        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
+        "name": "balanceOf",
+        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "burn",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "mint",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "to", "type": "address" },
+            { "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
+        "name": "transfer",
+        "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            { "internalType": "address", "name": "from", "type": "address" },
+            { "internalType": "address", "name": "to", "type": "address" },
+            { "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
+        "name": "transferFrom",
         "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
         "stateMutability": "nonpayable",
         "type": "function"
